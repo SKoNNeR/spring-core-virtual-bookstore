aaf215cd0a27e5eff52a0b1547c336c41de662c7
Francisco José Lamela López <skonner87@gmail.com>
2026-02-01 19:12:11 +0100

    add Spring Java config with component scanning

---
+++ spring-core-virtual-bookstore/src/dev/fjlamela/springcore/config/AppConfig.java
@@ -0,0 +1,10 @@
+package dev.fjlamela.springcore.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("dev.fjlamela.springcore")
+public class AppConfig {
+
+}
